5e8ac3ccc8d2c21ebea07459891c8f4423279d0e
cemil17 <cemil.kagan.7@gmail.com>
2021-09-16 18:23:08 +0300

    asciCode-find

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char asciValue = '5';
+
+            Console.WriteLine("Asci Code : {0} ", (int)asciValue);
+            
+            
+            Console.ReadLine();
+
+        }
+    }
+}
